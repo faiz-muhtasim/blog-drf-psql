e2eb68998decbb3b7d6b68ab05c1670f367e6e95
Faiz Muhtasim <faizmuhtasim438@gmail.com>
2026-03-13 17:28:37 +0600

    response thik kora (get chara data pass korbo na) Done

--- core/utils/response.py
+++ core/utils/response.py
@@ -1,20 +1,25 @@
 # core/utils/response.py
 
-def success_response(data=None, message="Success", code=200):
-    return {
-        "data": data,
-        "response_status": {
-            "success": True,
-            "code": code,
-            "message": message,
-        },
+def success_response(data=None, message="Success", code=200, include_data=False):
+    response = {}
+
+    if include_data and data is not None:
+        response["data"] = data
+
+    response["response_status"] = {
+        "success": True,
+        "code": code,
+        "message": message,
     }
 
-def error_response(data=None, message="Error", code=400):
+    return response
+
+
+def error_response(message="Error", code=400):
     return {
         "response_status": {
             "success": False,
             "code": code,
             "message": message,
-        },
+        }
     }
